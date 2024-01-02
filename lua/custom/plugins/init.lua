892d172f4f8b768330130171fcfdbc515665fc11
Faez <faez@psight.io>
2024-01-02 11:59:26 +0400

    Adding tmux navigator

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -35,14 +35,6 @@ local plugins = {
     "williamboman/mason-lspconfig.nvim",
   },
   {
-    "VonHeikemen/lsp-zero.nvim",
-    lazy = false,
-    branch = "v2.x",
-    config = function()
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
     "nvimtools/none-ls.nvim",
     lazy = false,
     config = function()
@@ -113,43 +105,41 @@ local plugins = {
     end,
   },
   {
-    {
-      "stevearc/conform.nvim",
-      event = { "BufWritePre" },
-      cmd = { "ConformInfo" },
-      keys = {
-        {
-          -- Customize or remove this keymap to your liking
-          "<leader>f",
-          function()
-            require("conform").format { async = true, lsp_fallback = true }
-          end,
-          mode = "",
-          desc = "Format buffer",
-        },
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>f",
+        function()
+          require("conform").format { async = true, lsp_fallback = true }
+        end,
+        mode = "",
+        desc = "Format buffer",
       },
-      -- Everything in opts will be passed to setup()
-      opts = {
-        -- Define your formatters
-        formatters_by_ft = {
-          lua = { "stylua" },
-          python = { "isort", "black" },
-          javascript = { { "prettierd", "prettier" } },
-        },
-        -- Set up format-on-save
-        format_on_save = { timeout_ms = 500, lsp_fallback = true },
-        -- Customize formatters
-        formatters = {
-          shfmt = {
-            prepend_args = { "-i", "2" },
-          },
-        },
-      },
-      init = function()
-        -- If you want the formatexpr, here is the place to set it
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-      end,
     },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
   {
     "nvim-telescope/telescope.nvim",
